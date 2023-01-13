264a073150a2fd30eb512a6cc936b1b4b89a38d8
coby5502 <coby5502@gmail.com>
2023-01-13 18:30:07 +0900

    [ADD] guideLabel 추가 (#16)

--- FoodBowl/Screens/Feed/SetStoreViewController.swift
+++ FoodBowl/Screens/Feed/SetStoreViewController.swift
@@ -13,17 +13,20 @@ import Then
 final class SetStoreViewController: BaseViewController {
     // MARK: - property
 
-    private let screenText = UILabel().then {
-        $0.textColor = .red
-        $0.text = "SetStore"
+    private let guideLabel = UILabel().then {
+        $0.numberOfLines = 0
+        let guide = NSAttributedString(string: "피드를 작성할\n가게를 찾아보세요.").withLineSpacing(10)
+        $0.attributedText = guide
+        $0.font = UIFont.preferredFont(forTextStyle: .title3, weight: .medium)
     }
 
     // MARK: - life cycle
 
     override func render() {
-        view.addSubview(screenText)
-        screenText.snp.makeConstraints {
-            $0.centerX.centerY.equalToSuperview()
+        view.addSubviews(guideLabel)
+        guideLabel.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top).inset(20)
+            $0.leading.equalToSuperview().inset(20)
         }
     }
 }
