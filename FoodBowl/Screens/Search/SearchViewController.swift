0cd04dae3c5a42b7b951e194d111e0f99fd58659
coby5502 <57849386+coby5502@users.noreply.github.com>
2023-01-18 01:55:23 +0900

    [ADD] 랜덤 피드 컬렉션뷰 추가 (#36)

--- FoodBowl/Screens/Search/SearchViewController.swift
+++ FoodBowl/Screens/Search/SearchViewController.swift
@@ -11,6 +11,18 @@ import SnapKit
 import Then
 
 final class SearchViewController: BaseViewController {
+    private enum Size {
+        static let headerHeight: CGFloat = 240
+        static let cellWidth: CGFloat = (UIScreen.main.bounds.size.width - 8) / 3
+        static let cellHeight: CGFloat = cellWidth
+        static let collectionInset = UIEdgeInsets(top: 0,
+                                                  left: 0,
+                                                  bottom: 20,
+                                                  right: 0)
+    }
+
+    private var feeds: [String] = ["가나", "다라", "마바", "사아", "자차", "사아", "자차", "사아", "자차", "사아", "자차", "사아", "자차", "사아", "자차"]
+
     // MARK: - property
 
     private lazy var searchBarButton = SearchBarButton().then {
@@ -26,19 +38,56 @@ final class SearchViewController: BaseViewController {
         $0.addAction(action, for: .touchUpInside)
     }
 
+    private let collectionViewFlowLayout = UICollectionViewFlowLayout().then {
+        $0.scrollDirection = .vertical
+        $0.sectionInset = Size.collectionInset
+        $0.itemSize = CGSize(width: Size.cellWidth, height: Size.cellHeight)
+        $0.minimumLineSpacing = 4
+        $0.minimumInteritemSpacing = 4
+    }
+
+    private lazy var listCollectionView = UICollectionView(frame: .zero, collectionViewLayout: collectionViewFlowLayout).then {
+        $0.backgroundColor = .clear
+        $0.dataSource = self
+        $0.delegate = self
+        $0.showsVerticalScrollIndicator = false
+        $0.register(FeedThumnailCollectionViewCell.self, forCellWithReuseIdentifier: FeedThumnailCollectionViewCell.className)
+    }
+
     // MARK: - life cycle
 
     override func render() {
-        view.addSubviews(searchBarButton)
+        view.addSubviews(searchBarButton, listCollectionView)
 
         searchBarButton.snp.makeConstraints {
             $0.top.equalTo(view.safeAreaLayoutGuide)
             $0.leading.trailing.equalToSuperview().inset(20)
             $0.height.equalTo(50)
         }
+
+        listCollectionView.snp.makeConstraints {
+            $0.top.equalTo(searchBarButton.snp.bottom).offset(10)
+            $0.leading.trailing.bottom.equalToSuperview()
+        }
     }
 
     override func setupNavigationBar() {
         navigationController?.isNavigationBarHidden = true
     }
+}
+
+extension SearchViewController: UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_: UICollectionView, numberOfItemsInSection _: Int) -> Int {
+        return feeds.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: FeedThumnailCollectionViewCell.className, for: indexPath) as? FeedThumnailCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+
+        return cell
+    }
+
+    func collectionView(_: UICollectionView, didSelectItemAt _: IndexPath) {}
 }
