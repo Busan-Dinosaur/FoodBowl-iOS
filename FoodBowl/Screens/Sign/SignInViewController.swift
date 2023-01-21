88185c97254df91a4346853e626cdfded6804aa1
coby5502 <57849386+coby5502@users.noreply.github.com>
2023-01-22 02:29:04 +0900

    [ADD] 온보딩/회원가입/로그인 화면 추가 (#46)

---
+++ FoodBowl/Screens/Sign/SignInViewController.swift
@@ -0,0 +1,39 @@
+//
+//  SignInViewController.swift
+//  FoodBowl
+//
+//  Created by COBY_PRO on 2023/01/22.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class SignInViewController: BaseViewController {
+    var delegate: SetCategoryViewControllerDelegate?
+    private let categories = Category.allCases
+
+    // MARK: - property
+
+    private let guideLabel = UILabel().then {
+        $0.text = "로그인"
+        $0.font = UIFont.preferredFont(forTextStyle: .title3, weight: .medium)
+    }
+
+    // MARK: - life cycle
+
+    override func render() {
+        view.addSubviews(guideLabel)
+
+        guideLabel.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top).inset(20)
+            $0.leading.equalToSuperview().inset(20)
+        }
+    }
+
+    override func setupNavigationBar() {
+        super.setupNavigationBar()
+        title = "로그인"
+    }
+}
