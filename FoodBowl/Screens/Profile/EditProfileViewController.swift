44f52554d1d5a051127e261fb0514b4da8538227
coby5502 <57849386+coby5502@users.noreply.github.com>
2023-01-26 21:17:20 +0900

    [FEAT] 프로필수정화면 생성 및 버튼 연결 (#49)

---
+++ FoodBowl/Screens/Profile/EditProfileViewController.swift
@@ -0,0 +1,136 @@
+//
+//  EditProfileViewController.swift
+//  FoodBowl
+//
+//  Created by COBY_PRO on 2023/01/26.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+import YPImagePicker
+
+final class EditProfileViewController: BaseViewController {
+    private var profileImage: UIImage = ImageLiteral.defaultProfile
+
+    // MARK: - property
+
+    private lazy var profileImageView = UIImageView().then {
+        $0.image = ImageLiteral.defaultProfile
+        $0.layer.cornerRadius = 50
+        $0.layer.masksToBounds = true
+        $0.clipsToBounds = true
+        $0.isUserInteractionEnabled = true
+        $0.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(tappedProfileImageView)))
+    }
+
+    private let nicknameLabel = UILabel().then {
+        $0.text = "닉네임"
+        $0.font = UIFont.preferredFont(forTextStyle: .body, weight: .medium)
+    }
+
+    private let nicknameField = UITextField().then {
+        let attributes = [
+            NSAttributedString.Key.foregroundColor: UIColor.grey001,
+            NSAttributedString.Key.font: UIFont.preferredFont(forTextStyle: .body, weight: .regular)
+        ]
+
+        $0.backgroundColor = .white
+        $0.attributedPlaceholder = NSAttributedString(string: "8자 이내 한글 또는 영문", attributes: attributes)
+        $0.autocapitalizationType = .none
+        $0.layer.cornerRadius = 12
+        $0.layer.masksToBounds = true
+        $0.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 0))
+        $0.leftViewMode = .always
+        $0.clipsToBounds = false
+        $0.clearButtonMode = .always
+        $0.makeBorderLayer(color: .grey002)
+    }
+
+    private lazy var signUpButton = MainButton().then {
+        $0.label.text = "완료"
+
+        let action = UIAction { [weak self] _ in
+            let tabbarViewController = UINavigationController(rootViewController: TabbarViewController())
+            tabbarViewController.modalPresentationStyle = .fullScreen
+            tabbarViewController.modalTransitionStyle = .crossDissolve
+            DispatchQueue.main.async {
+                self?.present(tabbarViewController, animated: true)
+            }
+        }
+        $0.addAction(action, for: .touchUpInside)
+    }
+
+    // MARK: - life cycle
+
+    override func render() {
+        view.addSubviews(profileImageView, nicknameLabel, nicknameField, signUpButton)
+
+        profileImageView.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top).inset(20)
+            $0.centerX.equalToSuperview()
+            $0.width.height.equalTo(100)
+        }
+
+        nicknameLabel.snp.makeConstraints {
+            $0.top.equalTo(profileImageView.snp.bottom).offset(20)
+            $0.leading.equalToSuperview().inset(20)
+        }
+
+        nicknameField.snp.makeConstraints {
+            $0.top.equalTo(nicknameLabel.snp.bottom).offset(10)
+            $0.leading.trailing.equalToSuperview().inset(20)
+            $0.height.equalTo(60)
+        }
+
+        signUpButton.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview().inset(20)
+            $0.bottom.equalTo(view.safeAreaLayoutGuide).inset(20)
+            $0.height.equalTo(60)
+        }
+    }
+
+    override func setupNavigationBar() {
+        super.setupNavigationBar()
+        title = "프로필 수정"
+    }
+
+    @objc private func tappedProfileImageView(_: UITapGestureRecognizer) {
+        var config = YPImagePickerConfiguration()
+        config.onlySquareImagesFromCamera = true
+        config.library.defaultMultipleSelection = false
+        config.library.mediaType = .photo
+        config.startOnScreen = YPPickerScreen.library
+        config.shouldSaveNewPicturesToAlbum = true
+        config.albumName = "FoodBowl"
+
+        let titleAttributes = [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 17, weight: .regular)]
+        let barButtonAttributes = [NSAttributedString.Key.font: UIFont.preferredFont(forTextStyle: .headline, weight: .regular)]
+        UINavigationBar.appearance().titleTextAttributes = titleAttributes // Title fonts
+        UIBarButtonItem.appearance().setTitleTextAttributes(barButtonAttributes, for: .normal) // Bar Button fonts
+        config.wordings.libraryTitle = "갤러리"
+        config.wordings.cameraTitle = "카메라"
+        config.wordings.next = "다음"
+        config.wordings.cancel = "취소"
+        config.colors.tintColor = .mainPink
+
+        let picker = YPImagePicker(configuration: config)
+
+        picker.didFinishPicking { [unowned picker] items, cancelled in
+            if !cancelled {
+                let images: [UIImage] = items.compactMap { item in
+                    if case let .photo(photo) = item {
+                        return photo.image
+                    } else {
+                        return nil
+                    }
+                }
+                self.profileImage = images[0]
+                self.profileImageView.image = images[0]
+            }
+            picker.dismiss(animated: true, completion: nil)
+        }
+        present(picker, animated: true, completion: nil)
+    }
+}
