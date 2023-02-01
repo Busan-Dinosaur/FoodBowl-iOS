3ebb1626032273ad023a752abe35c1090fe1ce97
coby5502 <57849386+coby5502@users.noreply.github.com>
2023-02-01 17:01:20 +0900

    [FEAT] 애플로그인 적용 (#70)

---
+++ FoodBowl/Network/Storage/UserDefaultHandler.swift
@@ -0,0 +1,34 @@
+//
+//  UserDefaultHandler.swift
+//  FoodBowl
+//
+//  Created by COBY_PRO on 2023/02/01.
+//
+
+import Foundation
+
+struct UserDefaultHandler {
+    static func clearAllData() {
+        UserData<Any>.clearAll()
+    }
+    
+    static func setIsLogin(isLogin: Bool) {
+        UserData.setValue(isLogin, forKey: .isLogin)
+    }
+    
+    static func setUserID(userID: String) {
+        UserData.setValue(userID, forKey: .userID)
+    }
+    
+    static func setAccessToken(accessToken: String) {
+        UserData.setValue(accessToken, forKey: .accessToken)
+    }
+    
+    static func setRefreshToken(refreshToken: String) {
+        UserData.setValue(refreshToken, forKey: .refreshToken)
+    }
+    
+    static func setNickname(nickname: String) {
+        UserData.setValue(nickname, forKey: .nickname)
+    }
+}
