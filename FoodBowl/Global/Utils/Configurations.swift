32cc4d12a233bdbc2f3539497a99c732bfe5823e
coby5502 <57849386+coby5502@users.noreply.github.com>
2023-08-09 23:50:14 +0900

    [FEAT] Keychain 추가 (#107)

---
+++ FoodBowl/Global/Utils/Configurations.swift
@@ -0,0 +1,31 @@
+//
+//  Configurations.swift
+//  FoodBowl
+//
+//  Created by COBY_PRO on 2023/08/09.
+//
+
+import Foundation
+
+enum ConfigurationsKey: String {
+    case nonce = "Nonce"
+}
+
+@propertyWrapper
+struct Configurations<K, T> where K: RawRepresentable, K.RawValue == String {
+    let key: K
+    let defaultValue: T
+
+    init(key: K, defaultValue: T) {
+        self.key = key
+        self.defaultValue = defaultValue
+    }
+
+    var wrappedValue: T {
+        guard let value = Bundle.main.object(forInfoDictionaryKey: key.rawValue) as? T
+        else {
+            fatalError("Invalid Configuration Value")
+        }
+        return value
+    }
+}
