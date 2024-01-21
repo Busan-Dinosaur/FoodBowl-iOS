37226578099796a9b14046e9a37760f2abb92b49
Coby <coby5502@gmail.com>
2024-01-22 02:16:00 +0900

    [CHORE] 텍스트필드뷰 수정 (#129)

--- FoodBowl/Presentation/Scene/ProfileScene/View/UpdateProfileView.swift
+++ FoodBowl/Presentation/Scene/ProfileScene/View/UpdateProfileView.swift
@@ -219,6 +219,11 @@ extension UpdateProfileView: UITextFieldDelegate {
         return true
     }
     
+    func textFieldShouldClear(_ textField: UITextField) -> Bool {
+        self.completeButton.isEnabled = self.getIsEnabled()
+        return true
+    }
+    
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
