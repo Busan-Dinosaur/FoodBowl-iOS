9e751f3a68a89e54d07564c1c7f6ff1cb6da5e0e
Coby <coby5502@gmail.com>
2024-02-14 05:12:47 +0900

    [CHORE] 장소 버튼 스타일 추가 (#135)

--- FoodBowl/Presentation/Common/Component/Button/ImageButton/MyPlaceButton.swift
+++ FoodBowl/Presentation/Common/Component/Button/ImageButton/MyPlaceButton.swift
@@ -34,6 +34,7 @@ final class MyPlaceButton: UIButton {
     override init(frame _: CGRect) {
         super.init(frame: .init(origin: .zero, size: .init(width: 30, height: 30)))
         self.configureUI()
+        self.setupAction()
     }
 
     @available(*, unavailable)
@@ -42,8 +43,27 @@ final class MyPlaceButton: UIButton {
     }
 
     private func configureUI() {
-        self.setImage(ImageLiteral.place.resize(to: CGSize(width: 20, height: 20)).withRenderingMode(.alwaysTemplate), for: .normal)
-        self.tintColor = .mainPink
-        self.backgroundColor = .mainBackgroundColor
+        self.isSelected = false
+    }
+    
+    private func setupAction() {
+        self.addTarget(self, action: #selector(handleTouchDown), for: .touchDown)
+        self.addTarget(self, action: #selector(handleTouchUpInside), for: .touchUpInside)
+        self.addTarget(self, action: #selector(handleTouchUpOutside), for: .touchUpOutside)
+    }
+    
+    @objc private func handleTouchDown() {
+        // 사용자가 버튼을 누르는 순간의 색상 변경
+        self.isSelected = true
+    }
+    
+    @objc private func handleTouchUpInside() {
+        // 사용자가 버튼을 떼는 순간의 색상 복원
+        self.isSelected = false
+    }
+    
+    @objc private func handleTouchUpOutside() {
+        // 사용자가 버튼 밖에서 손을 떼는 순간의 색상 복원
+        self.isSelected = false
     }
 }
